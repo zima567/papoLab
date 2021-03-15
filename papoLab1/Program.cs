4e7df596490ad4547b0c5e1fbb54a9c63215056c
Daniel Moise Montezima <mdanielmoise@gmail.com>
2021-03-15 16:59:33 +0300

    Добавьте файлы проекта.

---
+++ papoLab1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace papoLab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
